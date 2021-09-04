f6a231b947ad5dd6fec44b03f14464bda9929fae
Vikarchuk <roma.vikarchuk@gmail.com>
2021-09-04 11:41:47 +0300

    hot fix

--- resources/views/admin/messages/edit.blade.php
+++ resources/views/admin/messages/edit.blade.php
@@ -5,41 +5,42 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Create message</div>
-                    <div class="row">
-                        <div class="col-lg-12 margin-tb">
-                            <div class="pull-right">
-                                <a class="btn btn-primary" href="{{ route('admin.messages.index') }}" title="Go back"> Go back </a>
+                    <div class="card-body">
+                        <div class="card-header">Create message</div>
+                        <div class="row">
+                            <div class="col-lg-12 margin-tb">
+                                <div class="pull-right">
+                                    <a class="btn btn-primary" href="{{ route('admin.messages.index') }}" title="Go back"> Go back </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    @include('parts.errorsChecking')
-                    <form action="{{ route('admin.messages.update',['message' => $message->id]) }}" method="POST" >
-                        @method('PUT')
-                        @csrf
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <strong>Name:</strong>
-                                    <input type="text" name="name" class="form-control" value="{{ old('name') ?? $message->name }}" required maxlength="100">
-                                </div>
-                                <div class="form-group">
-                                    <strong>Email:</strong>
-                                    <input type="email" name="email" class="form-control" value="{{ old('email') ?? $message->email }}" required>
-                                </div>
-                                <div class="form-group">
-                                    <strong>Message:</strong>
-                                    <textarea name="description" class="form-control" maxlength="1000" required>
+                        @include('parts.errorsChecking')
+                        <form action="{{ route('admin.messages.update',['message' => $message->id]) }}" method="POST" >
+                            @method('PUT')
+                            @csrf
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Name:</strong>
+                                        <input type="text" name="name" class="form-control" value="{{ old('name') ?? $message->name }}" required maxlength="100">
+                                    </div>
+                                    <div class="form-group">
+                                        <strong>Email:</strong>
+                                        <input type="email" name="email" class="form-control" value="{{ old('email') ?? $message->email }}" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <strong>Message:</strong>
+                                        <textarea name="description" class="form-control" rows="5" cols="30" maxlength="1000" required>
                                         {{ old('description') ?? $message->description }}
                                     </textarea>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </div>
-                        </div>
-                    </form>
-
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -5,38 +5,43 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Create message</div>
-                    <div class="row">
-                        <div class="col-lg-12 margin-tb">
-                            <div class="pull-right">
-                                <a class="btn btn-primary" href="{{ route('messages.index') }}" title="Go back"> Go back </a>
+                    <div class="card-body">
+                        <div class="card-header">Create message</div>
+                        <div class="row">
+                            <div class="col-lg-12 margin-tb">
+                                <div class="pull-right">
+                                    <a class="btn btn-primary" href="{{ route('messages.index') }}" title="Go back"> Go back </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12">
+                            <div class="box">                                
+                                @include('parts.errorsChecking')
+                                <form action="{{ route('messages.store') }}" method="POST" >
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-12 col-md-12">
+                                            <div class="form-group">
+                                                <strong>Name:</strong>
+                                                <input type="text" name="name" class="form-control" placeholder="Enter your name" required maxlength="100">
+                                            </div>
+                                            <div class="form-group">
+                                                <strong>Email:</strong>
+                                                <input type="email" name="email" class="form-control" placeholder="Enter your email" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <strong>Message:</strong>
+                                                <textarea name="description" class="form-control" rows="5" cols="30" placeholder="Enter your message here" maxlength="1000" required></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
-                    @include('parts.errorsChecking')
-                    <form action="{{ route('messages.store') }}" method="POST" >
-                        @csrf
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <strong>Name:</strong>
-                                    <input type="text" name="name" class="form-control" placeholder="Name" required maxlength="100">
-                                </div>
-                                <div class="form-group">
-                                    <strong>Email:</strong>
-                                    <input type="email" name="email" class="form-control" placeholder="Email" required>
-                                </div>
-                                <div class="form-group">
-                                    <strong>Message:</strong>
-                                    <textarea name="description" class="form-control" placeholder="Select your message here" maxlength="1000" required></textarea>
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </div>
-                        </div>
-                    </form>
-
                 </div>
             </div>
         </div>
